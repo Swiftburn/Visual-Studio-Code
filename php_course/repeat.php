9ff9ca2d1d2e549ff41111758dd4794524cbcd55
Swiftburn <badolfo4@gmail.com>
2022-10-30 06:44:45 -0500

    Fixed Print On 0 and Printing One Extra Amount

--- php_course/repeat.php
+++ php_course/repeat.php
@@ -1,18 +1,20 @@
 <?php
+#Variables declared
 $word = "Hello";
 $num = 3;
 
 
+#Repeat function based on number
 function repeat($word,$num){
     $i = 0;
-    if ($num >= 0){
+    if ($num > 0){
         while ($i < $num){
             echo $word;
             $i++;
         }
-    }else echo " ";
+    }else echo " "; #If number is 0 or less, it prints nothing
 }
 
 
-repeat($word,$num);
+repeat($word,$num); #Calls function
 ?>
