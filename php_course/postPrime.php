003be41503e05152966fd28e10446ba98509291f
Swiftburn <badolfo4@gmail.com>
2022-11-02 20:34:54 -0500

    Checks and Outputs if Number is Prime or Not

--- php_course/postPrime.php
+++ php_course/postPrime.php
@@ -1,5 +1,25 @@
 <?php
 
+if(isset($_POST['submit'])){
+   $number = $_POST['number'];
+   function prime($number){
+    if ($number == 1)
+        return 0;
+
+    for ($i = 2; $i <= $number/2; $i++){
+        if ($number % $i == 0)
+            return 0;
+    }
+    return 1;
+   } 
+   
+   $check = prime($number);
+   if ($check == 1)
+    echo "Prime";
+   else
+    echo "Not Prime";
+}
+
 ?>
 
 
