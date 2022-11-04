a7212fd7a6cea53d67de2bbbb640b3b9f713b5e9
Swiftburn <badolfo4@gmail.com>
2022-11-03 21:33:01 -0500

    Added Cities in Array and Added Cities From File

---
+++ php_course/pointsOfInterest/index.php
@@ -0,0 +1,53 @@
+<?php
+
+// I want to know the best places to go in a city...
+
+    if(isset($_GET["city"])){ // If I have a valid city...
+        $city = $_GET["city"]; // Get city and save in attractions variable
+
+        $attractions = get_list_from_file();
+
+        if(strcmp($city, "all") == 0){
+            header("Content-type: application/json");
+            print_r(json_encode($attractions));
+        }
+        else{
+            header("Content-type: text/plain");
+
+            if(array_key_exists($city, $attractions)){ //Go to attractions and print attractions in that city
+                echo "The best place to visit in ". $city ." is ". $attractions[$city]. ".\n";
+            }
+            else{
+                echo "I did not find that on my list! Perhaps we can add it someday.";
+            }
+        }
+    }
+
+    function get_list_from_file(){
+        $contents = file("cities.txt", FILE_IGNORE_NEW_LINES);
+        $attractions = array();
+        foreach ($contents as $line){
+            $parts = explode(",", $line);
+            $attractions[$parts[0]] = $parts[1];
+        }
+        return $attractions;
+    }
+
+    // function get_list(){
+    //     /* Hard code all the places and the attractions in those cities... */
+    //     $attractions = array( "Seattle" => "Space Needle",
+    //                             "New York" => "Ellis Island",
+    //                             "Boston" => "Boston Harbor",
+    //                             "Philadelphia" => "Valley Forge",
+    //                             "Detroit" => "The Henry Ford Museum",
+    //                             "Paris" => "Eiffel Tower",
+    //                             "Vancouver BC" => "Stanley Park",
+    //                             "Tokyo" => "Mt. Fuji",
+    //                             "Beijing" => "Great Wall",
+    //                             "London" => "Big Ben");
+
+    //     return $attractions;
+        
+    // }
+
+?>
