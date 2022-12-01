ca2b73010a07540e4a39e5f2f61a4879ea68ef02
Swiftburn <badolfo4@gmail.com>
2022-12-01 04:05:11 -0600

    Created a Fully Connected & Functional Form

--- Resume_Final/contact.php
+++ Resume_Final/contact.php
@@ -1,34 +1,93 @@
 <?php include 'inc/header.php' ?>
 
 
-<h1>Contact Me</h1>
-<form action="" method="POST">
-    <div class="contact">
-        <label for="">Name</label>
-        <input type="text" class="form-control" id="" name="" placeholder="">
-        <div class="invalid-feedback">
+<?php
+  $name = $email = $body = $reply = '';
+  $nameErr = $emailErr = $bodyErr = '';
 
+  //Form submission
+  if(isset($_POST['submit'])){
+    //Validate the name
+    if(empty($_POST['name'])){
+      $nameErr = 'Name is required!';
+    } else {
+      $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+
+
+    //Validate the email
+    if(empty($_POST['email'])){
+      $emailErr = 'Email is required!';
+    } else {
+      $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    }
+
+    
+    //Validate the body
+    if(empty($_POST['body'])){
+      $bodyErr = 'Feedback is required!';
+    } else {
+      $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+
+    
+    if(empty($nameErr) && empty($emailErr) && empty($bodyErr)){
+      //If no errors, add to database...
+      $sql = "INSERT INTO feedback(name, email, body) VALUES ('$name', '$email', '$body')";
+  
+      if(mysqli_query($conn, $sql)){
+        //If all went well...
+        $reply = "Thank you $name for your message. I will email you at $email shortly!";
+      } else {
+        //If error exists...
+        echo 'Error: ' . mysqli_error($conn);
+      }
+    }
+  }
+
+
+  ?>
+
+
+
+
+
+
+
+
+
+<h1>Contact Me</h1>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
+    <div class="contact">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control <?php echo $nameErr? 'is-invalid' : null; ?>" id="name" name="name" placeholder="Enter your name">
+        <div class="invalid-feedback">
+            <?php echo $nameErr; ?>
         </div>
     </div>
 
     <div class="contact">
-        <label for="">Email</label>
-        <input type="text" class="form-control" id="" name="" placeholder="">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control <?php echo $emailErr? 'is-invalid' : null; ?>" id="email" name="email" placeholder="Enter your email">
         <div class="invalid-feedback">
-
+            <?php echo $emailErr; ?>
         </div>
     </div>
 
     <div class="contact">
         <label for="body" class="form-label">Message</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your message"></textarea>
+        <textarea class="form-control <?php echo $bodyErr? 'is-invalid' : null; ?>" id="body" name="body" placeholder="Enter your message"></textarea>
         <div class="invalid-feedback">
-
+            <?php echo $bodyErr; ?>
         </div>
     </div>
 
     <div>
         <input type="submit" name="submit" value="Send">
+    </div>
+
+    <div>
+        <?php echo $reply; ?>
     </div>
 </form>
 
