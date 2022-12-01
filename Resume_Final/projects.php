555dee2ddd297385d9ac4c75100b698589f4ff06
Swiftburn <badolfo4@gmail.com>
2022-12-01 06:02:08 -0600

    Added Recent Github Projects

--- Resume_Final/projects.php
+++ Resume_Final/projects.php
@@ -2,9 +2,35 @@
 
 
 <div id="projects">
-        <h1>Projects I have worked On</h1>
-        
+<h1>Github Recent Projects</h1>
+    <div>
+        <h3><a href="https://github.com/Swiftburn/Web_Programming_2022/tree/master/CompoundinterestCalc">Compound Interest Calculator</a></h3>
+            <li>A compound interest calculator that outputs a table with the results.</li>
+            <li>Started with html and built a form, from that I went on to javascript to build a compound interest calculator, and finally added some CSS to make it look more pleasing.</li>
     </div>
+        
+    <div>
+        <h4><a href="https://github.com/Swiftburn/Web_Programming_2022/tree/master/clockTower">Clock Tower</a></h4>
+            <li>Copied an existing website just via sight</li>
+            <li>I started with the easier things such as text (paragraphs, sections, headers, etc.) then I moved on to the more difficult to gauge css and did my best to get as close as possible.</li>
+    </div>
+        
+    <div>
+        <h4><a href="https://github.com/Swiftburn/Web_Programming_2022/tree/master/Resume%20Project">Midterm Resume</a></h4> 
+            <li>Resume website created to test our skills with html and css</li>
+            <li>I did go a bit far with this creation because I found some exciting new css features to make things stand out</li>
+    </div>
+        
+    <div>
+        <h4><a href="https://github.com/Swiftburn/Web_Programming_2022/tree/master/Resume_Final">This Website!</a></h4>
+            <li>This is a continuation of the Resume Website midterm but we had to built upon it with php and javascript!</li>
+            <li>Yes this very website is a project I worked on!</li>
+            <li>Since I already had the basics set, I just had to split the resume into 4 different pages, add an API through javascript, create an SQL database connection via php, and finally decorate it with css but sparingly this time.</li>
+    </div>
+        
+        
+        
+</div>
 
 
 
