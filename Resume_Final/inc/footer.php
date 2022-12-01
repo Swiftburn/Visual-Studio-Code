44234520de0434de2e89c52b243348ae0b1f2272
Swiftburn <badolfo4@gmail.com>
2022-11-30 22:07:51 -0600

    Added Separate Files For Header/Footer

---
+++ Resume_Final/inc/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <a href="https://www.linkedin.com/in/adolfo-barrientos-040b53255/">LinkedIn</a>
+    |
+    <a href="https://github.com/Swiftburn">Github</a>
+</footer>
+</div>
+
+
+
+
+
+
+</body>
+</html>
---
+++ Resume_Final/inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Adolfo Barrientos">
+    <title>Adolfo Barrientos</title>
+    <link rel="stylesheet" href="inc/style.css">
+    
+</head>
+<body>
+    <nav>
+        <div class="weather">
+            <script src="inc/weather.js"></script>
+        </div>
+        
+        
+        <div>
+            <a href="">example</a>
+        </div>
+    </nav>
