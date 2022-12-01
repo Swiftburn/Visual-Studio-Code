d0057104f25e80e743e4be09b17aec7c78fca763
Swiftburn <badolfo4@gmail.com>
2022-12-01 06:47:04 -0600

    Added Comments and Cleaned Up Code

--- Resume_Final/inc/weather.js
+++ Resume_Final/inc/weather.js
@@ -1,36 +1,29 @@
-
+    //Grabs API Key
     let weather = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c70328c914dd083562afe14476f90dd4"
-        console.log("ok!")
+        console.log("api key ok!")
     
+        //Fetch API
         fetch(weather)
         .then(response => {
-            return response.json();
-           ({})
+            return response.json(); //returns json
         })
         .then(data => {
+            //Show data in console
             console.log(data)
-            
-            city = data;
-            temp = data;
 
+            //Finds specific data and stores data in respective variables
             city = data.city.name + ' '
             temp = data.list['0'].main.temp + ' '
             humid = data.list['0'].main.humidity
+
+            //Locates respective classes with respective name, then before the end tag, adds data
             document.querySelector('.city').insertAdjacentHTML('beforeend', city)
             document.querySelector('.temp').insertAdjacentHTML('beforeend', temp)
             document.querySelector('.humid').insertAdjacentHTML('beforeend', humid)
         })
-        .catch(error => console.log('Error ' + error.message))
-
-
-        // .then(response => {return response.json()})
-        // .then(
-        //     data => console.log(data)
-        // )
-        // .catch(err => {console.log('Errors: ' + err.message)})
-        // document.write(data)
+        .catch(error => console.log('Error ' + error.message)) //catch any errors
         
 
 
 
-console.log("outside function is fine")
+console.log("last check is fine")
