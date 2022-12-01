babb1a20d5344313543f42eb4ea87f8f8e4fd39d
Swiftburn <badolfo4@gmail.com>
2022-12-01 02:02:16 -0600

    Added Projects to projects.php

--- Resume_Final/projects.php
+++ Resume_Final/projects.php
@@ -1,4 +1,15 @@
-<?php
-echo "hello agaain again";
-echo "hello again";
-?>
+<?php include 'inc/header.php' ?>
+
+
+<div id="projects">
+        <h2>Projects I have worked in:</h2>
+        <ul>
+            <li><strong>Freelance (Video Editor) &#8211; 2019-present</strong> &#8211; Communicate with clients on specific goals and objectives using a video editor, Davinci Resolve and Photoshop</li><br>
+
+            <li><strong>Discord Community (Server Owner) &#8211; 2016-2020</strong> &#8211; Recruited, made final decisions, and created events using Discord</li><br>
+
+            <li><strong>Self Employed, Entrepreneur &#8211; 2017-2018</strong> &#8211; Generated ~1K profit selling games and refreshments locally and tracked revenue and expenses through Microsoft Office</li><br>
+
+            <li><strong>Russellville Pope County Library (Library Assistant) &#8211; 2014-2016</strong> &#8211; Facilitated effective communication between staff members and Spanish-speaking library patrons and provided customer service to 1000+ of county residents and local community</li>
+        </ul>
+    </div>
--- Resume_Final/welcome.php
+++ Resume_Final/welcome.php
@@ -5,11 +5,11 @@
     
 
 
-<div id="background">
+
     
     <div id="picture">
         <!--picture goes here-->
-        <img src="img/selfie.jpg" alt="selfie" height="500px" width="100%">
+        <img src="img/selfie.jpg" alt="selfie">
     </div>
 
     <div id="intro">
@@ -17,21 +17,10 @@
         <h2>Information Technology</h2>
         <h2>Arkansas Tech University</h2>
         <h3>
-            I love to solve problems and I'm down to earth. Some things I'm interested in are web development, app development, databases, and automation. Every chance I get in video games, I will automate everything that is repeated.
+            I love to solve problems and I'm down to earth. Some things I'm interested in are web development, app development, databases, and automation. I am most comfortable doing back end 
         </h3>  
     </div>
 
-    <div id="projects">
-        <h2>Projects I have worked in:</h2>
-        <ul>
-            <li><strong>Freelance (Video Editor) &#8211; 2019-present</strong> &#8211; Communicate with clients on specific goals and objectives using a video editor, Davinci Resolve and Photoshop</li><br>
-
-            <li><strong>Discord Community (Server Owner) &#8211; 2016-2020</strong> &#8211; Recruited, made final decisions, and created events using Discord</li><br>
-
-            <li><strong>Self Employed, Entrepreneur &#8211; 2017-2018</strong> &#8211; Generated ~1K profit selling games and refreshments locally and tracked revenue and expenses through Microsoft Office</li><br>
-
-            <li><strong>Russellville Pope County Library (Library Assistant) &#8211; 2014-2016</strong> &#8211; Facilitated effective communication between staff members and Spanish-speaking library patrons and provided customer service to 1000+ of county residents and local community</li>
-        </ul>
-    </div>
+    
 
     <?php include 'inc/footer.php'; ?>
